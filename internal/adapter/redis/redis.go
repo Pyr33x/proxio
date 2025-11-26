6fc28762c27174fd2aaa52d1e51986e4e2ea45b4
pyr33x <pyrexrj@gmail.com>
2025-11-26 11:57:21 +0330

    feat: initialize redis constructor adapter

---
+++ internal/adapter/redis/redis.go
@@ -0,0 +1,31 @@
+package redis
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/pyr33x/proxy/pkg/config"
+	"github.com/redis/go-redis/v9"
+	"go.uber.org/zap"
+)
+
+type Adapter struct {
+	rdb *redis.Client
+}
+
+func New(ctx context.Context, cfg *config.Redis, logger *zap.Logger) *Adapter {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%s", cfg.Host, cfg.Port),
+		Username: cfg.Username,
+		Password: cfg.Password,
+		DB:       cfg.Database,
+	})
+
+	if err := rdb.Ping(ctx).Err(); err != nil {
+		logger.Error("failed to connect to redis", zap.Error(err))
+		return nil
+	}
+
+	logger.Info("attached redis", zap.String("state", "connected"))
+	return &Adapter{rdb: rdb}
+}
