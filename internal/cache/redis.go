f620c743fb1f0f08dd757227b8eb77a81b9f13e5
pyr33x <pyrexrj@gmail.com>
2025-12-06 00:28:42 +0330

    feat(cache): implement redis over caching interface

---
+++ internal/cache/redis.go
@@ -0,0 +1,28 @@
+package cache
+
+import (
+	"context"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type redisStore struct {
+	client *redis.Client
+}
+
+func NewRedisStore(client *redis.Client) *redisStore {
+	return &redisStore{client: client}
+}
+
+func (r *redisStore) Get(ctx context.Context, key string) ([]byte, error) {
+	return r.client.Get(ctx, key).Bytes()
+}
+
+func (r *redisStore) Set(ctx context.Context, key string, value []byte, ttl time.Duration) error {
+	return r.client.Set(ctx, key, value, ttl).Err()
+}
+
+func (r *redisStore) Clear(ctx context.Context) error {
+	return r.client.FlushAll(ctx).Err()
+}
